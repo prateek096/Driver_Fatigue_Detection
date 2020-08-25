2202fe99259a1c07e27131518e42faaf22edbe92
Gautam Prateek <56034286+prateek096@users.noreply.github.com>
2020-08-25 23:09:26 +0530

    Update and rename eye_detection_single_image.py to eye_detection_image.py

--- eye_detection_image.py
+++ eye_detection_image.py
@@ -7,12 +7,8 @@ eye_cascade = cv.CascadeClassifier("haarcascades/haarcascade_eye.xml")
 
 img = cv.imread('images/test.jpeg')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-#Detect faces 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-#Draw a rectangle on the face
-#Eye Detection
 for (x,y,w,h) in faces:
     cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
@@ -20,7 +16,6 @@ for (x,y,w,h) in faces:
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
 
-    #Detect eyes
     eyes = eye_cascade.detectMultiScale(roi_gray)
 
     for (ex,ey,ew,eh) in eyes:
