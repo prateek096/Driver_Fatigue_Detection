90faa339cd29f53ae091a4c5e85c00d0bf60a2d4
Gautam Prateek <56034286+prateek096@users.noreply.github.com>
2020-08-25 23:08:27 +0530

    Update eye_detection_webcam_video.py

--- eye_detection_webcam_video.py
+++ eye_detection_webcam_video.py
@@ -1,19 +1,15 @@
-
-#Import necessary libraries
 import numpy as np
 import cv2 as cv
 
-#Load face cascade and hair cascade from haarcascades folder
 face_cascade = cv.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
 eye_cascade = cv.CascadeClassifier("haarcascades/haarcascade_eye.xml")
 
-#Capture video from webcam
 video_capture = cv.VideoCapture(0)
 
-#Read all frames from webcam
+#Read all frames 
 while True:
     ret, frame = video_capture.read()
-    frame = cv.flip(frame,1) #Flip so that video feed is not flipped, and appears mirror like.
+    frame = cv.flip(frame,1) 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -32,7 +28,6 @@ while True:
 
     if(cv.waitKey(1) & 0xFF == ord('q')):
         break
-
-#Finally when video capture is over, release the video capture and destroyAllWindows
+        
 video_capture.release()
 cv.destroyAllWindows()
