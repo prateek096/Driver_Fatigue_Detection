77035133dfbfdeafd5ea4e011b9833be18040c3e
Gautam Prateek <56034286+prateek096@users.noreply.github.com>
2020-08-25 23:06:59 +0530

    Update and rename drowsiness_detect.py to fatigue_detect.py

--- fatigue_detect.py
+++ fatigue_detect.py
@@ -14,17 +14,13 @@ pygame.mixer.music.load('audio/alert.wav')
 
 #Minimum threshold of eye aspect ratio 
 eye_aspect_ratio_threshold = 0.4
-
-#Minimum consecutive frames for alarm to be triggered
 frames_threshold = 50
-
-#Counts no. of consecutuve frames below threshold value
 count_frames = 0
 
 #Load face cascade 
 face_cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
 
-#This function calculates and return eye aspect ratio
+#Calculates and return eye aspect ratio
 def eye_aspect_ratio(eye):
     A = distance.euclidean(eye[1], eye[5])
     B = distance.euclidean(eye[2], eye[4])
@@ -37,70 +33,62 @@ def eye_aspect_ratio(eye):
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-#Extract indexes of facial landmarks for the left and right eye
+#Landmarks of the left and right eye
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS['left_eye']
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
 
-#Start webcam video capture
 video_capture = cv2.VideoCapture(0)
 
-#Give some time for camera to initialize(not required)
-time.sleep(2)
-
 while(True):
-    #Read each frame and flip it, and convert to grayscale
     ret, frame = video_capture.read()
     frame = cv2.flip(frame,1)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #Detect facial points through detector function
+    #Detect facial points
     faces = detector(gray, 0)
 
     #Detect faces through haarcascade_frontalface_default.xml
     face_rectangle = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-    #Draw rectangle around each face detected
+    
     for (x,y,w,h) in face_rectangle:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-    #Detect facial points
     for face in faces:
 
         shape = predictor(gray, face)
         shape = face_utils.shape_to_np(shape)
 
-        #Get array of coordinates of leftEye and rightEye
+        #array of coordinates
         leftEye = shape[lStart:lEnd]
         rightEye = shape[rStart:rEnd]
 
-        #Calculate aspect ratio of both eyes
+        #Calculate aspect ratio
         leftEyeAspectRatio = eye_aspect_ratio(leftEye)
         rightEyeAspectRatio = eye_aspect_ratio(rightEye)
 
         eyeAspectRatio = (leftEyeAspectRatio + rightEyeAspectRatio) / 2
 
-        #Use hull to remove convex contour discrepencies and draw eye shape around eyes
+        #Remove convex contour
         leftEyeHull = cv2.convexHull(leftEye)
         rightEyeHull = cv2.convexHull(rightEye)
         cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 
-        #Detect if eye aspect ratio is less than threshold
+        #Main Algo
         if(eyeAspectRatio < eye_aspect_ratio_threshold):
             count_frames += 1
             #If no. of frames is greater than threshold frames,
             if count_frames >= frames_threshold:
                 pygame.mixer.music.play(-1)
-                cv2.putText(frame, "You are Drowsy", (150,200), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 2)
+                cv2.putText(frame, "Take Rest,YOU ARE SLEEPY", (150,200), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,255), 2)
         else:
             pygame.mixer.music.stop()
             count_frames = 0
 
-    #Show video feed
+    #video feed
     cv2.imshow('Video', frame)
     if(cv2.waitKey(1) & 0xFF == ord('q')):
         break
 
-#Finally when video capture is over, release the video capture and destroyAllWindows
 video_capture.release()
 cv2.destroyAllWindows()
